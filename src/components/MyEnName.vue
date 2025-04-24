2f25b0abcbc9a3b0c29cbf9f9c9ac63e1d672739
Mitchell Yuen <mitchell@ozow.com>
2025-04-24 13:05:53 +0200

    fix: name not displaying on ios mobile browsers

--- src/components/MyEnName.vue
+++ src/components/MyEnName.vue
@@ -1,5 +1,11 @@
 <template>
-  <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2560 428.98">
+  <svg
+    id="Layer_1"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 2560 428.98"
+    preserveAspectRatio="xMidYMid meet"
+    style="width: 100%; height: auto; display: block;"
+  >
     <text transform="translate(118.95 361.69)" style="fill: #f70399; font-family: BebasKai, 'Bebas Kai'; font-size: 429.69px;">
       <tspan x="0" y="0" style="letter-spacing: 0em;">D</tspan>
       <tspan x="260" y="0">ANIELLE</tspan>
